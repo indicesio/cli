ea51e623ed08fe1e1a462f1774a07184e82bfad3
Mandeep Cheema <mandeep@cheema.uk>
2026-04-16 23:06:13 +0100

    Include `traceparent` on progenitor's outbound requests

--- src/client/generated/mod.rs
+++ src/client/generated/mod.rs
@@ -3,3 +3,14 @@
 #![allow(unused_qualifications)]
 
 include!(concat!(env!("OUT_DIR"), "/indices_openapi.rs"));
+
+impl ClientHooks<()> for Client {
+    async fn pre<E>(
+        &self,
+        request: &mut reqwest::Request,
+        _info: &OperationInfo,
+    ) -> std::result::Result<(), Error<E>> {
+        crate::telemetry::inject_trace_context(request.headers_mut());
+        Ok(())
+    }
+}
